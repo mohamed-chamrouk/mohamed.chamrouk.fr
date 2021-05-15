214bd7716c5fcb98b1906d6b1e96d1b119acd405
Mohamed CHAMROUK <mohamed.chamrouk@telecom-sudparis.eu>
2021-05-15 02:03:03 +0200

    Revert "Ajout du style du bouton login et du fichier requirements.txt pour pip"
    This reverts commit 7e6e45fdaa38b1c9c595f698c63f3cce20b220bd.

--- websitemohamed.py
+++ websitemohamed.py
@@ -1,12 +1,7 @@
 from flask import Flask, render_template
 from flask import has_request_context, request
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://snow:myp@s5w0r4@212.111.40.134:5432/website_db"
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
 
 @app.route("/portfolio")
 def portfolio():
